cd7b7442ec80cdc23254651d4cb8e8b2ae209b77
Alex Hampton <ahampton5158@gmail.com>
2019-08-21 11:49:19 +0900

    Add JS_5 and HTML_5

---
+++ Basic-JavaScript-Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,44 @@
+document.write((typeof "45") + "\r");
+document.write(0 / 0 + "\n");
+document.write(isNaN(4) + "\n\r");
+document.write("\n");
+document.write(isNaN(NaN));
+document.write("\n");
+document.write(isNaN("red"));
+document.write("\n");
+document.write(2E310);
+document.write("\n");
+document.write(-2E311);
+document.write("\n");
+document.write(10 > 2);
+document.write("\n");
+document.write(10 <= 2);
+document.write("\n");
+document.write("5" + 10);
+document.write("\n");
+document.write(15 + 7 == 23);
+document.write("\n");
+document.write(15 + 7 == 22);
+
+
+document.getElementById("para1").innerHTML = 2e311;
+document.getElementById("para2").innerHTML = -2e311;
+document.getElementById("para4").innerHTML = 10 > 2;
+document.getElementById("para6").innerHTML = 10 <= 2;
+document.getElementById("para8").innerHTML = "5" + 10;
+document.getElementById("para10").innerHTML = 15 + 7 == 23;
+document.getElementById("para12").innerHTML = 15 + 7 == 22;
+document.getElementById("para14").innerHTML = 10 === 10;
+document.getElementById("para16").innerHTML = "10" === 8;
+document.getElementById("para18").innerHTML = "10" === 10;
+document.getElementById("para20").innerHTML = 7 === 8;
+document.getElementById("para22").innerHTML = 10 > 4 && 5 < 10;
+document.getElementById("para24").innerHTML = 10 > 4 && 5 > 10;
+document.getElementById("para26").innerHTML = 8 > 2 || 3 > 7;
+document.getElementById("para28").innerHTML = 8 < 2 || 3 > 7;
+document.getElementById("para30").innerHTML = !(5 > 10);
+console.log(2 + 2);
+console.log("10" + 5);
+console.log(5 + "10");
+console.log(isNaN(5 - 4));
+
